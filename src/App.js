f6b90eba8b1a6aa02a1695246e1060851ad88874
Satrya Wiguna <satrya@freshcms.net>
2022-09-13 01:09:26 +0800

    IAM2-004 Add registration

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./components/admin/Dashboard";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
       <Route path="/admin/dashboard" element={<Dashboard />} />
     </Routes>
   );
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { updateAuth } from "../features/authSlice";
 import Message from "./Message";
+import { useSelector } from "react-redux";
 
-const Login = () => {
+const Register = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
 
   const { logged } = useSelector((state) => state.auth);
 
@@ -17,34 +15,27 @@ const Login = () => {
     }
   }, []);
 
+  const [fullName, setFullName] = useState("");
+  const [nickName, setNickName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const [messages, setMessages] = useState([]);
 
-  const actionLogin = async (e) => {
+  const actionRegister = async (e) => {
     e.preventDefault();
 
     try {
-      const login = await axios.post("/auth/login", {
+      const register = await axios.post("/register", {
+        full_name: fullName,
+        nick_name: nickName,
         email: email,
         password: password,
+        confirm_password: confirmPassword,
       });
 
-      if (login.data.type == "SUCCESS") {
-        dispatch(
-          updateAuth({
-            logged: true,
-            fullName: login.data.fullName,
-            nickName: login.data.nickName,
-            email: login.data.email,
-          })
-        );
-
-        localStorage.setItem("access_token", login.data.token.access_token);
-        localStorage.setItem("refresh_token", login.data.token.refresh_token);
-
-        navigate("/admin/dashboard", { replace: true });
-      }
+      if (register.data.type == "SUCCESS") navigate("/");
     } catch (error) {
       if (error.response) {
         setMessages(error.response.data.messages);
@@ -63,7 +54,31 @@ const Login = () => {
           <div className="columns is-centered">
             <div className="column is-4-desktop">
               <Message messages={messages} closeMessage={closeMessage} />
-              <form onSubmit={actionLogin} className="box">
+              <form className="box" onSubmit={actionRegister}>
+                <div className="field mt-3">
+                  <label className="label">Full Name</label>
+                  <div className="controls">
+                    <input
+                      type="text"
+                      className="input"
+                      placeholder="Full Name"
+                      value={fullName}
+                      onChange={(e) => setFullName(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="field mt-3">
+                  <label className="label">Nick Name</label>
+                  <div className="controls">
+                    <input
+                      type="text"
+                      className="input"
+                      placeholder="Nick Name"
+                      value={nickName}
+                      onChange={(e) => setNickName(e.target.value)}
+                    />
+                  </div>
+                </div>
                 <div className="field mt-3">
                   <label className="label">Email</label>
                   <div className="controls">
@@ -89,14 +104,25 @@ const Login = () => {
                   </div>
                 </div>
                 <div className="field mt-3">
+                  <label className="label">Confirm Password</label>
+                  <div className="controls">
+                    <input
+                      type="password"
+                      className="input"
+                      placeholder="Consfirm Password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="field mt-3">
                   <button className="button is-success is-fullwidth">
-                    Login
+                    Register
                   </button>
                 </div>
                 <div className="field mt-3">
                   <small>
-                    Don't have account? <Link to={"/register"}>Click here</Link>{" "}
-                    for registration
+                    Back to login? <Link to={"/"}>Click here</Link>
                   </small>
                 </div>
               </form>
@@ -108,4 +134,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
